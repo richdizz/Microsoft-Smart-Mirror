93b642fc0c9d47d8f4e580436a1db92742c16416
Richard diZerega <ridize@microsoft.com>
2017-09-20 14:45:15 -0700

    Added MailSentimentPart

--- SmartMirror/Controls/TrendingPart.xaml.cs
+++ SmartMirror/Controls/TrendingPart.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using SmartMirror.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -22,6 +24,19 @@ namespace SmartMirror.Controls
         public TrendingPart()
         {
             this.InitializeComponent();
+        }
+
+        public async override void Initialize(User user, bool isEditMode = false)
+        {
+            base.Initialize(user, isEditMode);
+
+            // Query trending information for the user
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + user.AuthResults.access_token);
+            using (var resp = await client.GetAsync($"https://graph.microsoft.com/beta/me/insights/trending"))
+            {
+            }
         }
     }
 }
